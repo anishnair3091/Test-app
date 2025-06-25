9b0a019710b2ad623eb231cb7809010d5928d3ae
anishnair3091 <anish.nair3091@gmail.com>
2025-06-25 16:24:45 +0400

    Add files via upload

---
+++ Sign-in.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import pandas as pd
+from pathlib import Path 
+import pickle   
+
+# User Authentication
+
+user_data= pd.read_csv("user_data.csv")
+
+
+def user_type():
+    data = user_data[user_data['Username'] == name]
+    for user in data.User_type:
+        user_type= user
+        return user_type
+
+users= []
+user_ids= []
+usertypes= []
+for user, ids, types in zip(user_data.Username, user_data.User_id, user_data.User_type):
+    users.append(user)
+    user_ids.append(ids)
+    usertypes.append(types)
+
+# Load credentials except passwords
+
+names= users
+usernames= user_ids
+usertype= usertypes
+
+# Load passwords
+
+file_path = Path("hashed_pw.pkl")
+with file_path.open('rb') as file:
+    hashed_passwords= pickle.load(file)
+
+
+# Transform credentials as a dictionary
+
+credentials = {"usernames":{}}
+for un, name, pswd, char in zip(usernames, names, hashed_passwords, usertype):   
+    user_dict = {"name":name, "password" : pswd, "role":char}
+    credentials["usernames"].update({un:user_dict})
+
+authenticator= stauth.Authenticate(credentials, "", "", cookie_expiry_days=0)
+
+
+name, authentication_status, username= authenticator.login('main', 'Login')
+
+if authentication_status == False:
+    st.error("Invalid Username/Password")
+
+
+
+if authentication_status == True:
+    st.sidebar.write(f'Welcome **{name}**')
+    if user_type() == 'customer':
+        st.switch_page("Home-page.py")
+    elif user_type() == 'supplier':
+        st.switch_page("Supplier-Home-page.py")
+    
+    elif user_type() == 'Admin':
+        st.switch_page("admin_page.py")
+    else:
+        st.error("Invalid username/password")
+        
+    
+
+
+
+if st.sidebar.button('Main'):
+    st.switch_page('Main_page-copy.py')
