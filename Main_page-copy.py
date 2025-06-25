9e57cc4df07663e2a5fe031778869c7800c28d17
anishnair3091 <anish.nair3091@gmail.com>
2025-06-25 15:30:49 +0400

    Update Main_page-copy.py

--- Main_page-copy.py
+++ Main_page-copy.py
@@ -33,8 +33,7 @@ col1, col2, col3, col4, col5, col6, col7, col8, col9, col10= st.columns(10)
 with col8:
 	st.page_link('https://www.bmts.ae', label= 'WEBSITE', icon= "🌍")
 		
-with col9:
-	st.page_link('pages/Sign-in.py', label= 'SIGN-IN', icon=":material/login:")
+
 
 
 
