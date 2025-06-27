628593c0f39059e5a5b6f20bc76536f814a7e8b3
anishnair3091 <anish.nair3091@gmail.com>
2025-06-27 18:24:14 +0400

    Update Home-page.py

--- pages/Home-page.py
+++ pages/Home-page.py
@@ -78,17 +78,7 @@ page_bg_img= f"""
 }}
 </style>
 """
-st.markdown(page_bg_img, unsafe_allow_html=True)
 
-page_bg_head= f"""
-<p>P2S</p>
-<style>
-.st-emotion-cache-gkoddq{{
-text-align: left;
-color: #7B8181;
-}}
-"""
-st.markdown(page_bg_head, unsafe_allow_html=True)
 
 Home= st.Page('pages/Home-page.py', title= 'Home page', icon= '🏡')
 
