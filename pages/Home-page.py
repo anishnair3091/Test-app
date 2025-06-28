23abf115eb687e012c3ef264aa9b2868828931cb
anishnair3091 <anish.nair3091@gmail.com>
2025-06-28 09:19:58 +0400

    Update Home-page.py

--- pages/Home-page.py
+++ pages/Home-page.py
@@ -214,7 +214,8 @@ if usertype() == 'customer' or usertype() == 'Admin':
 					if st.button("**Reports**", icon='📝', use_container_width=True, type='tertiary'):
 						st.switch_page("pages/reports.py")
 
-if st.sidebar.button('Logout'):
-	st.logout()
+if st.user.is_logged_in:
+	if st.sidebar.button('Logout'):
+		st.logout()
 
 
